9aa9b723ca8e442e361ed320272c13a80d18590d
lblaszka <mlblaszka@gmail.com>
2019-03-10 16:08:33 +0100

    Add ProductConvert implementation

--- src/main/java/com/github/lblaszka/pointofsale/product/ProductConverterImpl.java
+++ src/main/java/com/github/lblaszka/pointofsale/product/ProductConverterImpl.java
@@ -1,17 +1,36 @@
 package com.github.lblaszka.pointofsale.product;
 
+import com.github.lblaszka.pointofsale.barcode.BarCode;
+
 public class ProductConverterImpl implements ProductConverter
 {
     @Override
-    public ProductDTO convert( Product product )
+    public ProductDTO convert( Product product ) throws ProductConverterException
     {
-        return null;
+        if( product == null )
+            throw new ProductConverterException( "ProductConverter: product is null!" );
+        return new ProductDTO(
+                product.getLabel(),
+                new BarCode( product.getBarCode() ),
+                product.getPrice()
+        );
     }
 
 
     @Override
-    public Product convert( ProductDTO productDTO )
+    public Product convert( ProductDTO productDTO ) throws ProductConverterException
     {
-        return null;
+        if( productDTO == null )
+            throw new ProductConverterException( "ProductConverter: productDTO is null!" );
+
+        if( !( productDTO.getBarCode().getBarCodeObject() instanceof String ) )
+            throw new ProductConverterException( "ProductConverter: BarCode object is not String class!" );
+
+        return new Product(
+                new Long( 0L ),
+                productDTO.getLabel(),
+                (String) productDTO.getBarCode().getBarCodeObject(),
+                productDTO.getPrice()
+        );
     }
 }
