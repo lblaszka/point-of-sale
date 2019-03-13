dfe2b37a1fa9c96c55fa72bec324ddb65e83e6d9
lblaszka <mlblaszka@gmail.com>
2019-03-13 16:12:39 +0100

    Create DeviceImpl

--- src/main/java/com/github/lblaszka/pointofsale/device/DeviceImpl.java
+++ src/main/java/com/github/lblaszka/pointofsale/device/DeviceImpl.java
@@ -3,11 +3,19 @@ package com.github.lblaszka.pointofsale.device;
 import com.github.lblaszka.pointofsale.barcode.BarCodeContainer;
 import com.github.lblaszka.pointofsale.displaylcd.DisplayLCD;
 import com.github.lblaszka.pointofsale.displaylcd.DisplayLCDElementImplProduct;
+import com.github.lblaszka.pointofsale.displaylcd.DisplayLCDElementImplTotalPrice;
+import com.github.lblaszka.pointofsale.pricecalculator.PriceCalculator;
 import com.github.lblaszka.pointofsale.printer.Printer;
+import com.github.lblaszka.pointofsale.printer.PrinterElement;
+import com.github.lblaszka.pointofsale.printer.PrinterElementImplProduct;
+import com.github.lblaszka.pointofsale.printer.PrinterElementImplTotalPrice;
+import com.github.lblaszka.pointofsale.product.Product;
 import com.github.lblaszka.pointofsale.product.ProductDTO;
 import com.github.lblaszka.pointofsale.product.ProductService;
 import com.github.lblaszka.pointofsale.scanner.Scanner;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -18,16 +26,19 @@ public class DeviceImpl implements Device
     private DisplayLCD displayLCD;
     private Printer printer;
     private ProductService productService;
+    private PriceCalculator priceCalculator;
 
     private List<ProductDTO> productList = new LinkedList<>(  );
 
 
-    public DeviceImpl( Scanner scanner, DisplayLCD displayLCD, Printer printer, ProductService productService )
+    public DeviceImpl( Scanner scanner, DisplayLCD displayLCD, Printer printer, ProductService productService, PriceCalculator priceCalculator, List<ProductDTO> productList )
     {
         this.scanner = scanner;
         this.displayLCD = displayLCD;
         this.printer = printer;
         this.productService = productService;
+        this.priceCalculator = priceCalculator;
+        this.productList = productList;
     }
 
 
@@ -60,6 +71,10 @@ public class DeviceImpl implements Device
                 putOnDisplayInvalidBarCode();
         }
 
+        BigDecimal totalPrice = getTotalPrice( productList );
+        displayLCD.putOnScreen( new DisplayLCDElementImplTotalPrice( totalPrice ) );
+        printSalesReceipt( productList, totalPrice );
+
     }
 
     private void putOnDisplayInvalidBarCode()
@@ -70,6 +85,34 @@ public class DeviceImpl implements Device
     private void putOnDisplayProductNotFound()
     {
         displayLCD.putOnScreen( () -> { return "Invalid bar-code";} );
+    }
+
+    private BigDecimal getTotalPrice( List<ProductDTO> _productList )
+    {
+        List<BigDecimal> priceList = new ArrayList<>( _productList.size() );
+        for( ProductDTO product : _productList )
+        {
+            priceList.add( product.getPrice() );
+        }
+
+        return priceCalculator.getTotalPrice( priceList );
+    }
+
+    private void printSalesReceipt( List<ProductDTO> _productList, BigDecimal _totalPrice )
+    {
+        List<PrinterElement> printerElements = new ArrayList<>( _productList.size() + 1);
+        for( ProductDTO product : _productList )
+        {
+            printerElements.add(
+                new PrinterElementImplProduct(
+                        product.getLabel(),
+                        product.getPrice()
+                )
+            );
+        }
+        printerElements.add( new PrinterElementImplTotalPrice( _totalPrice ) );
+
+        printer.printElements( printerElements );
     }
 
 
