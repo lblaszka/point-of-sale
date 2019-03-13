7ab9c69c0b45b0be4cd0ef06b86af5560a074910
lblaszka <mlblaszka@gmail.com>
2019-03-13 15:58:56 +0100

    Create tests for PriceCalculator

---
+++ src/main/java/com/github/lblaszka/pointofsale/pricecalculator/PriceCalculatorImpl.java
@@ -0,0 +1,13 @@
+package com.github.lblaszka.pointofsale.pricecalculator;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PriceCalculatorImpl implements PriceCalculator
+{
+    @Override
+    public BigDecimal getTotalPrice( List<BigDecimal> priceList )
+    {
+        return null;
+    }
+}
